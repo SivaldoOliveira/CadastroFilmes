54fc3e1d15518d0e27ad60ef308c262ab587ddd6
Sivaldo Oliveira <oliveira.smo@outlook.com>
2024-08-11 23:15:45 -0300

    Project Created

---
+++ CadastroFilmes.Api/Program.cs
@@ -0,0 +1,29 @@
+using CadastroFilmes.IoC;
+
+namespace CadastroFilmes.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            Startup.Configure(builder.Configuration, builder.Services);
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
